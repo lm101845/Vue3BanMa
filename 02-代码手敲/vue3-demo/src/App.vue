a8058b21ddf6f0f8c44e27ba7ac6a12b290d378a
lm101845 <1018457683@qq.com>
2022-02-26 16:45:37 +0800

    第5集看完(5/200)

---
+++ 02-代码手敲/vue3-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <h1>{{msg}}</h1>
+        <button @click="say">按钮</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'App',
+        //1.组合API的起点，将来的组合API的代码，基本上在这里
+        //2.可以理解为：在beforeCreate钩子执行前执行，组件实例创建前就执行了
+        //3.因为组件实例还没创建，所以setup中不能用this,使用了也是undefined
+        //4.模板中需要使用的数据和函数，需要在setup中返回(要有return,且返回的值要为对象)
+        //5.setup函数中还可以传参，我们在学父子通讯时可以用到
+        setup(){
+            console.log('setup在beforeCreate之前执行了,且this是undefined',this);   //undefined
+            //数据
+            const msg = 'hi vue3'
+            //函数
+            const say = ()=>{
+                console.log('你好啊');
+            }
+            return {msg,say}
+            //return的值为对象，你可以往对象里面丢很多的数据和函数
+        },
+        beforeCreate(){
+            console.log('beforeCreate',this);
+        }
+    }
+</script>
---
+++ 02-代码手敲/vue3-demo/src/App的optionAPI.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div>鼠标位置：</div>
+    <div>X轴：{{x}}</div>
+    <div>Y轴：{{y}}</div>
+    <hr>
+    <div>{{count}} <button @click="add()">自增</button></div>  
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      x: 0,
+      y: 0,
+      count: 0
+    }
+  },
+  mounted() {
+    document.addEventListener('mousemove', this.move)
+  },
+  methods: {
+    move(e) {
+      this.x = e.pageX
+      this.y = e.pageY
+    },
+    add () {
+        this.count++
+    }    
+  },
+  destroyed() {
+    document.removeEventListener('mousemove', this.move)
+  }
+}
+</script>
